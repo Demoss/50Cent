2e62e0bc72e235e78f30b1d72923e18eddb93e15
Demoss <intetstellar2@gmail.com>
2022-11-16 17:40:09 +0200

    F back

--- backend/app/app.go
+++ backend/app/app.go
@@ -3,19 +3,17 @@ package app
 import (
 	"50Cent/backend/config"
 	"50Cent/backend/internal/controllers"
-	"50Cent/backend/internal/infrastructure/email"
-	"50Cent/backend/internal/repositories"
-	"50Cent/backend/internal/service"
 )
 
 func Run() error {
 	cfg := config.GetConfig()
 
-	mailclnt := email.NewSendgridClient(cfg)
+	/*mailclnt := email.NewSendgridClient(cfg)
 
-	rpsrs := repositories.NewRepository(nil, cfg, mailclnt)
-	srvcs := service.NewService(rpsrs, cfg)
-	cntrs := controllers.NewController(srvcs, cfg)
+	rpsrs := repositories.NewRepository(nil, nil, nil)
+	srvcs := service.NewService(nil, nil)
+	*/
+	cntrs := controllers.NewController(nil, nil)
 
 	err := cntrs.InitRouter(cfg.App.Port)
 	if err != nil {
