9dd8dd20cd9568bb87aea0a466b3b3786a10dfd6
MarianaZavarunska <89205808+MarianaZavarunska@users.noreply.github.com>
2023-02-21 23:46:33 +0200

    redo rounding the sum

--- frontend/src/api/credit/getCredit/mappers.ts
+++ frontend/src/api/credit/getCredit/mappers.ts
@@ -15,11 +15,11 @@ export function mapResponse(
   responseApi: GetCreditResponseApi,
 ): GetCreditResponse {
   return {
-    creditSum: Number((Math.round(responseApi.CreditSum * 100)/ 100).toFixed(2)),
+    creditSum: responseApi.CreditSum,
     creditTitle: responseApi.CreditTitle,
     creditDescription: responseApi.CreditDescription,
     creditTerm: responseApi.CreditTerm,
     creditRate: responseApi.CreditRate,
-    returnedInvestorMoney: Number((Math.round(responseApi.ReturnedInvestorMoney * 100)/ 100).toFixed(2)),
+    returnedInvestorMoney: responseApi.ReturnedInvestorMoney,
   };
 }
