cb8e5441c227ef18f7a649ec05194faed73d2653
Demoss <intetstellar2@gmail.com>
2023-03-13 12:14:25 +0200

    delete unused code

--- frontend/src/hooks/auth/useRefresh.ts
+++
@@ -1,9 +0,0 @@
-import { Api } from '@/api';
-import { useQuery } from 'react-query';
-
-export const useRefresh= (token: string) => {
-    const { data } = useQuery('userRefresh', async () => {
-        return await Api.refresh({refresh_token: token});
-    });
-    return { token: data?.token };
-};
